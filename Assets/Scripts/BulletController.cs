3d1ea077662d1a5056b36e8a78d724b72a242c97
shabbyhalfling6 <bradycjohnston@hotmail.com>
2017-02-16 12:14:37 +1000

    Initial Commit
    - Unity Project created
    - Player Movement completed
    - Player Controls completed
    - Player Animation and sprites completed

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+    public float bulletMoveSpeed = 6.0f;
+
+	void Update ()
+    {
+        transform.Translate(transform.forward * Time.deltaTime * bulletMoveSpeed);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float playerMoveSpeed = 2.0f;
+
+    protected Vector2 move;
+
+    private SpriteRenderer playerSpriteRenderer;
+    public GameObject bulletPrefab;
+
+    void Start()
+    {
+        playerSpriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    void Update ()
+    {
+        move = GetInputVector(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        this.transform.Translate(move * playerMoveSpeed);
+
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject bulletInstance = (GameObject)Instantiate(bulletPrefab, transform.position, transform.rotation);
+            bulletInstance.tag = "PlayerBullet";
+            bulletInstance.transform.Rotate(0, 0, Input.mousePosition.z);
+        } 
+    }
+
+    public Vector2 GetInputVector(float horizontalMove, float verticalMove)
+    {
+        FlipSprite(horizontalMove, verticalMove);
+
+        return new Vector2(horizontalMove, verticalMove);
+    }
+
+    private void FlipSprite(float horizontalMove, float veticalMove)
+    {
+        if (horizontalMove < 0)
+        {
+            playerSpriteRenderer.flipX = true;
+        }
+        else if (horizontalMove > 0)
+        {
+            playerSpriteRenderer.flipX = false;
+        }
+    }
+}
